e6b688a9e474aee7ceced5ac7f28316e07be693c
Xiaofeng <1244762145@qq.com>
2023-05-14 11:22:53 +0800

    分离出五个gloss的小样本集

--- Model comparison/dataset_grouping.py
+++ Model comparison/dataset_grouping.py
@@ -4,12 +4,17 @@
 # @Author :Xiaofeng
 import os
 import random
+import shutil
 
-import pandas as pd
-
-train_label_path = "E:/dataset/AUSTL/raw_data/train_data/train_labels.csv"
-val_label_path = "E:/dataset/AUSTL/raw_data/validation_data/ground_truth.csv"
-test_label_path = "E:/dataset/AUSTL/raw_data/test_data/ground_truth.csv"
+train_path = "E:/dataset/AUSTL/raw_data/train_data/train"
+validation_path = "E:/dataset/AUSTL/raw_data/validation_data/validation"
+test_path = "E:/dataset/AUSTL/raw_data/test_data/test"
+# train_label_path = "E:/dataset/AUSTL/raw_data/train_data/train_labels.csv"
+# val_label_path = "E:/dataset/AUSTL/raw_data/validation_data/validation_label.csv"
+# test_label_path = "E:/dataset/AUSTL/raw_data/test_data/test_label.csv"
+train_label_path = "data_label/train_labels.csv"
+val_label_path = "data_label/val_label.csv"
+test_label_path = "data_label/test_label.csv"
 
 
 def select_rows(label_path, random_numbers):
@@ -34,9 +39,32 @@ def select_rows(label_path, random_numbers):
     return selected_list
 
 
-if __name__ == '__main__':
+def create_label():
     random.seed(123)
     random_numbers = [random.randint(0, 225) for i in range(5)]
     # print(select_rows(train_label_path, random_numbers))
     print(select_rows(val_label_path, random_numbers))
     # print(select_rows(test_label_path, random_numbers))
+
+
+def copy_select_video(data_path, label_path, target_path):
+    label_file = open(label_path, 'r', encoding='utf-8')
+    for line in label_file.readlines():
+        lines = line.strip()
+        video_name = lines.split(',')[0] + '_color.mp4'
+        video_source = os.path.join(data_path, video_name)
+        # 创建目标文件夹
+        os.makedirs(target_path, exist_ok=True)
+        # 拼接目标文件路径
+        tartget_source = os.path.join(target_path, os.path.basename(video_source))
+        # 复制文件
+        shutil.copy(video_source, tartget_source)
+
+
+def copy_videos():
+    target_path = 'D:/Code/PythonCode/SignLanguageProject/CVPR21Chal-SLR_xiaofeng_SLR/data-prepare/data/raw_data/test/'
+    copy_select_video(test_path, test_label_path, target_path)
+
+
+if __name__ == '__main__':
+    copy_videos()
